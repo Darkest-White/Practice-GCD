54ac481852c78444d50525a27a5eb975d2b31ea9
Darkest-White <darkest.white.fox@gmail.com>
2023-04-14 18:10:23 +0300

    First commit

--- Practice-GCD/Main.cpp
+++ Practice-GCD/Main.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 int main()
 {
+    int x, y;
+    cin >> x >> y;
 
+    while (x != y) 
+    {
+        if (x > y) 
+        {
+            x = x - y;
+        }
+        else 
+        {
+            y = y - x;
+        }
+    }
+    cout << x << endl;;
 }
