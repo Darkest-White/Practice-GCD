849a3881da423a6637d36dfb9348952890bf7cb4
Darkest-White <darkest.white.fox@gmail.com>
2023-07-02 14:49:54 +0300

    Update

--- Practice-GCD/Main.cpp
+++ Practice-GCD/Main.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 
+int GCD(int x, int y)
+{
+	while (x != y)
+	{
+		if (x > y)
+		{
+			x = x - y;
+		}
+		else
+		{
+			y = y - x;
+		}
+	}
+	return x;
+}
+
+
 int main()
 {
-    int x, y;
-    cin >> x >> y;
-
-    while (x != y) 
-    {
-        if (x > y) 
-        {
-            x = x - y;
-        }
-        else 
-        {
-            y = y - x;
-        }
-    }
-    cout << x << endl;;
+	cout << GCD(243, 12) << endl;
 }
